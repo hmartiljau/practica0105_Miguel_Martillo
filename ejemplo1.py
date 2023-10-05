8baeb8171a8b102cfae88e94472cbb48dbc96689
hmartiljau <hmartiljau1@educacion.navarra.es>
2023-10-05 15:56:02 +0200

    Update ejemplo1.py

--- ejemplo1.py
+++ ejemplo1.py
@@ -1,6 +1,1 @@
-<<<<<<< HEAD
-#Escribir un programa que muestre por pantalla la cadena ¡Hola Mundo!.
-=======
-#Escribir un programa que muestre por pantalla la cadena ¡Hola Mundo!.
->>>>>>> 190bf4af1e7df354a5c30569231a889b5d616c56
 print('¡Hola Mundo!')
